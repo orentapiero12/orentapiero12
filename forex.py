8685aabc03b049993de644b201b31e0e3c8388f5
Oren Tapiero <ot@cuma.capital>
2021-10-10 15:51:01 +0300

    SS
    SSS

--- forex.py
+++ forex.py
@@ -5,16 +5,63 @@ import talib as ta
 import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
 
 sns.set()
 # %%
+def ohlc_plot(df,start = '2018',end = '2021',Log = False):
+    if Log:
+        fig = go.Figure(data=[go.Candlestick(x=df.loc[start:end].index,
+                        open=np.log(df.Open.loc[start:end]),
+                        high=np.log(df.High.loc[start:end]),
+                        low=np.log(df.Low.loc[start:end]),
+                        close=np.log(df.Close.loc[start:end]))])
+    else:
+        fig = go.Figure(data=[go.Candlestick(x=df.loc[start:end].index,
+                        open=(df.Open.loc[start:end]),
+                        high=(df.High.loc[start:end]),
+                        low=(df.Low.loc[start:end]),
+                        close=(df.Close.loc[start:end]))])
+
+
+    fig.update_layout(xaxis_rangeslider_visible=False)
+    fig.show()
+    return
+
+# %%
 with open('/Users/orentapiero/Data/Mid5T.pickle', 'rb') as handle:
     Mid = pickle.load(handle)
+    
+with open('/Users/orentapiero/Data/Bid5T.pickle', 'rb') as handle:
+    Bid = pickle.load(handle)
+
+with open('/Users/orentapiero/Data/Ask5T.pickle', 'rb') as handle:
+    Ask = pickle.load(handle)
 
 # %%
 pair = 'gbpusd'
-date = '2018-02-27'
+date = '2018'
 ohlc = Mid[pair].loc[date].dropna()
+ohlc.columns = ['Open','High','Low','Close']
+
+# %%
+ohlc_plot(ohlc,start = '2018-02-26',end = '2018-02-26',Log = False)
+
+# %%
+ohlc['H30mCmx'] = ohlc['Close'].rolling(6).max()
+ohlc['H30mCmn'] = ohlc['Close'].rolling(6).min()
+
+ohlc['H60mCmx'] = ohlc['Close'].rolling(12).max()
+ohlc['H60mCmn'] = ohlc['Close'].rolling(12).min()
+
+ohlc['H150mCmx'] = ohlc['Close'].rolling(30).max()
+ohlc['H150mCmn'] = ohlc['Close'].rolling(30).min()
+
+
+# %%
+ohlc.loc['2018-02-26',('H30mCmx','H30mCmn')].between_time('07:00','12:00').plot(figsize = (18,10),style = 'k--')
+ohlc.loc['2018-02-26',('H60mCmx','H60mCmn')].between_time('07:00','12:00').plot(figsize = (18,10),style = 'b--')
+ohlc.loc['2018-02-26',('H150mCmx','H150mCmn')].between_time('07:00','12:00').plot(figsize = (18,10),style = 'r--')
 
 # %%
 open = ohlc['open']
@@ -29,3 +76,5 @@ close.plot(ax=a[0])
 real.plot(ax = a[1])
 # %%
 
+
+# %%
